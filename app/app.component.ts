2e75dc2bd5148ac215a358f4c31c439b92bd553f
Hui Si Goh <huisigoh@gmail.com>
2016-05-29 21:24:01 +0800

    up to tutorial section 3.3

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,25 @@
 import {Component} from "@angular/core";
 
+import {User} from "./shared/user/user";
+
 @Component({
   selector: "my-app",
-  template: "<Label text='hello world'></Label>"
+  templateUrl: "pages/login/login.html",
+  styleUrls: ["pages/login/login-common.css", "pages/login/login.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  user: User;
+  isLoggingIn=true;
+  
+  constructor(){
+    this.user = new User();
+  }
+  
+  submit(){
+    alert("You're using: " + this.user.email);
+  }
+  
+  toggleDisplay(){
+    this.isLoggingIn = !this.isLoggingIn;
+  }
+}
